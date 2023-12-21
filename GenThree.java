dfa24b74482900ad999a214676409a3db382eef1
TheoTDM <theodore.daimaman@post.runi.ac.il>
2023-12-21 18:57:56 +0200

    Last 19:00

--- GenThree.java
+++ GenThree.java
@@ -6,9 +6,9 @@ public class GenThree {
 	public static void main(String[] args) {
 		int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-        int n1 = (int) (Math.random() * (b - a) + 1);
-        int n2 = (int) (Math.random() * (b - a) + 1);
-        int n3 = (int) (Math.random() * (b - a) + 1);
+        int n1 = (int) ((Math.random() * (b - a)) + a);
+        int n2 = (int) ((Math.random() * (b - a)) + a);
+        int n3 = (int) ((Math.random() * (b - a)) + a);
         System.out.println(n1);
         System.out.println(n2);
         System.out.println(n3);
